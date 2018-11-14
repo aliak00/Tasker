38f569c3b98b9f4ee7e9b1264539e1874307feb4
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-11-14 23:20:24 +0100

    adjust swiftformat

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,13 +2,18 @@
 
 if [[ -z "${TRAVIS}" ]]; then
     swiftformat \
-        Sources/ Tests  \
+        Sources/ Tests/  \
         --comments ignore \
         --ranges nospace \
         --self insert \
         --indent 4 \
         --allman false \
         --enable redundantGet \
+        --enable redundantLet \
+        --enable redundantReturn \
         --enable spaceAroundBraces \
+        --enable spaceInsideBraces \
+        --enable consecutiveBlankLines \
+        --enable linebreakAtEndOfFile \
         --header strip
 fi
