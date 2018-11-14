a5f2b0e0e402c4df300f80a95a2f9d715c473da0
Tor Arvid Lund <tor.arvid.lund@schibsted.com>
2018-11-14 14:34:47 +0100

    Add ScenarioBatchingTests

---
+++ Tests/Tasker/ScenarioBatchingTests.swift
@@ -0,0 +1,36 @@
+//
+// Copyright 2017 Ali Akhtarzada
+//
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+
+import XCTest
+import Tasker
+
+// This interceptor will take any task that is retriable, and re-execute it
+class BatchingInterceptor: TaskInterceptor {
+    func intercept<T: Task>(task _: inout T, currentBatchCount: Int) -> InterceptCommand {
+        return currentBatchCount < 9 ? .hold : .execute
+    }
+}
+
+class ScenarioBatchingTests: XCTestCase {
+
+    override func tearDown() {
+        ensure(kTaskSpyCounter.value).becomes(0)
+    }
+
+    func testShouldAllWork() {
+        let manager = TaskManagerSpy(interceptors: [BatchingInterceptor()])
+        for i in 0..<9 {
+            manager.add(task: TaskSpy { $0(.success(i)) })
+        }
+        ensure(manager.completionCallCount).stays(0)
+        manager.add(task: TaskSpy { $0(.success(())) })
+        ensure(manager.completionCallCount).becomes(10)
+    }
+}
