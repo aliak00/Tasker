6b9abf78bb8c6b0295c5414dd03fdb2d7130abfa
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-11-14 21:51:30 +0100

    Add xcode project

---
+++ Xcode/Tasker-macOS/Tasker_macOS.h
@@ -0,0 +1,18 @@
+//
+//  Tasker_macOS.h
+//  Tasker-macOS
+//
+//  Created by Ali Akhtarzada on 11/14/18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Tasker_macOS.
+FOUNDATION_EXPORT double Tasker_macOSVersionNumber;
+
+//! Project version string for Tasker_macOS.
+FOUNDATION_EXPORT const unsigned char Tasker_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tasker_macOS/PublicHeader.h>
+
+
