b69afcd5b97ba68fe700df0a4285a97a29cfd292
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-11-14 11:42:37 +0100

    fix up dir structure

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Tasker",
-            dependencies: []),
+            dependencies: [],
+            path: "./Sources"),
         .testTarget(
             name: "TaskerTests",
-            dependencies: ["Tasker"]),
+            dependencies: ["Tasker"],
+            path: "./Tests"),
     ]
 )
