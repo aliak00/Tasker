e30b39e4b7dd4fcf9f8c5eae7e7a5959b034d337
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2019-04-06 09:24:12 +0200

    make ios test script use ios 12.2

--- scripts/test-ios.sh
+++ scripts/test-ios.sh
@@ -3,5 +3,5 @@
 xcodebuild -scheme Tasker-iOS \
     -project Xcode/Tasker.xcodeproj \
     -sdk iphonesimulator \
-    -destination 'platform=iOS Simulator,name=iPhone 5s,OS=12.1' \
+    -destination 'platform=iOS Simulator,name=iPhone 5s,OS=12.2' \
     test | xcpretty
