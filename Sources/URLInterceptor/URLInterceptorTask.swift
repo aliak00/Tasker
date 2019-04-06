6a15cc04c7c83c97019a8aa392473d9da4a63ae7
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2019-04-06 14:52:18 +0200

    Add docs to URLIntercetorTask

--- Sources/URLInterceptor/URLInterceptorTask.swift
+++ Sources/URLInterceptor/URLInterceptorTask.swift
@@ -1,7 +1,12 @@
 import Foundation
 
-///
+/**
+ This is the `Task` object that is used by `URLInterceptor`. This is the type of the `Task` object
+ that would be passed in to `TaskInterceptor.intercept(...)` and `TaskReactor.shouldExecute(...)`
+ when using the `URLInterceptor`.
+ */
 public class URLInterceptorTask: Task {
+    /// The URLRequest object that was used to make the original request with `URLInterceptor.session`
     public var request: URLRequest
 
     var task: URLSessionDataTask?
@@ -10,10 +15,10 @@ public class URLInterceptorTask: Task {
         self.request = request
     }
 
-    ///
+    /// It's the tuple that's returned by called methods on URLSession
     public typealias SuccessValue = (data: Data?, response: URLResponse?, error: Error?)
 
-    ///
+    /// Executes the URLRequest
     public func execute(completion: @escaping CompletionCallback) {
         self.task = URLSession.shared.dataTask(with: self.request) { data, response, error in
             let value: SuccessValue = (data, response, error)
@@ -23,7 +28,7 @@ public class URLInterceptorTask: Task {
         self.task?.resume()
     }
 
-    ///
+    /// Calls URLSessionTask.cancel
     public func didCancel(with _: TaskError) {
         self.task?.cancel()
     }
